27378f7e010748105f1724ddae5e62813b849e1d
VoldMarius <ukusil@mail.ru>
2023-11-03 21:03:44 +0300

    All DONE!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import static org.example.Student.getAverageGrade;
 
